498fa95dc1264f7e12a5916df771feedbf8d87fb
dtomozawa <daisuke.tomozawa@gmail.com>
2023-02-19 07:34:06 +0900

    L14コミット

--- src/test/java/com/techacademy/HelloSpringBootController.java
+++ src/test/java/com/techacademy/HelloSpringBootController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime; // 追加
 import java.time.format.DateTimeFormatter; // 追加
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable; // 追加
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +15,19 @@ public class HelloSpringBootController {
         return "Hello SpringBoot!";
     }
 
-    // ----- 追加:ここから -----
     @GetMapping("now")
     public String dispTime() {
         String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
 
         return "現在時刻：" + now;
     }
+    // ----- 追加:ここから -----
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
+    }
     // ----- 追加:ここまで -----
+
 }
